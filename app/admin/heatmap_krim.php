c8ac1834444b85d8d1d5d47b7c49d078dc62c04f
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-10-22 20:13:43 +0200

    more presise heatmap

--- app/admin/heatmap_krim.php
+++ app/admin/heatmap_krim.php
@@ -54,95 +54,111 @@ if (!isset($_GET['acc_id'])) {
 
 <?php } else { ?>
 
-    <form method="post">
-        <label for="time">Velg tidsperspektiv</label>
-        <select name="time" id="cars">
-            <option value="1">Siste timen</option>
-            <option value="2">Siste 2 timer</option>
-            <option value="3">Siste 6 timer</option>
-            <option value="4">Siste 12 timer</option>
-            <option value="5">Siste 24 timer</option>
-        </select>
-        <input type="submit" name="update" value="Oppdater">
-    </form>
+    <div class="col-12 single">
+        <div class="col-8" style="padding-top: 0px;">
+            <div class="content" id="heatmap">
 
+                <?php
 
+                if (isset($_POST['time'])) {
+                    $times[0] = 0;
+                    $times[1] = 3600;
+                    $times[2] = 7200;
+                    $times[3] = 21600;
+                    $times[4] = 43200;
+                    $times[5] = 86400;
 
-    <div class="col-8 single">
-        <div class="content" id="heatmap">
+                    $query = $pdo->prepare('SELECT * FROM heatmap WHERE HM_page = :loc AND HM_acc_id = :id AND HM_date > :time');
+                    $query->execute(array(':loc' => "kriminalitet", ':id' => $_GET['acc_id'], ':time' => (time() -  $times[$_POST['time']])));
 
-            <?php
+                    foreach ($query as $row) {
+                        $x = $row['HM_x'] - 2.5;
+                        $y = $row['HM_y'] - 2.5;
 
-            if (isset($_POST['time'])) {
-                $times[0] = 0;
-                $times[1] = 3600;
-                $times[2] = 7200;
-                $times[3] = 21600;
-                $times[4] = 43200;
-                $times[5] = 86400;
-
-                $query = $pdo->prepare('SELECT * FROM heatmap WHERE HM_page = :loc AND HM_acc_id = :id AND HM_date > :time');
-                $query->execute(array(':loc' => "kriminalitet", ':id' => $_GET['acc_id'], ':time' => (time() -  $times[$_POST['time']])));
-
-                foreach ($query as $row) {
-                    $x = $row['HM_x'] - 2.5;
-                    $y = $row['HM_y'] - 2.5;
-
-                    echo "<div style='
+                        echo "<div style='
                           left: " . $x . ";
                           top: " . $y . ";
                           padding: 5px 5px;
                           background-color: rgba(50, 205, 50,.1);
                           border-radius: 10px;
                           position: absolute;'></div>";
+                    }
                 }
-            }
 
-
-            ?>
-            <img class="action_image" src="img/action/actions/kriminalitet.png">
-            <table>
-                <tr>
-                    <th style="width: 40%;">Handling</th>
-                    <th style="width: 20%;">Ventetid</th>
-                    <th style="width: 20%;">Sjanse</th>
-                    <th style="width: 20%;">Utbetaling</th>
-                </tr>
-
-                <?php
-
-                if (active_heist($_SESSION['ID'], $pdo) && heist_row($heist_id, 'HEIST_type', $pdo) == 1 && heist_row($heist_id, 'HEIST_status', $pdo) == 1) {
-                    $crime_amount = 9;
-                } else {
-                    $crime_amount = 8;
-                }
-                for ($i = 0; $i < $crime_amount; $i++) {
 
                 ?>
-                    <tr class="cursor_hover clickable-row" data-href="?side=kriminalitet&alt=<?php echo $i; ?>">
-                        <td>Lorem ipsum </td>
-                        <td>Lorem ipsum s</td>
-                        <td>Lorem ipsum %</td>
-                        <td>Lorem ipsum </td>
+                <img class="action_image" src="img/action/actions/kriminalitet.png">
+                <p style="text-align: center;" class="description">Byskatten i denne byen er <?php echo output_city_tax(get_city_tax(AS_session_row($_SESSION['ID'], 'AS_city', $pdo), $pdo)); ?></p>
+                <table>
+                    <tr>
+                        <th style="width: 40%;">Handling</th>
+                        <th style="width: 20%;">Ventetid</th>
+                        <th style="width: 20%;">Sjanse</th>
+                        <th style="width: 20%;">Utbetaling</th>
                     </tr>
-                <?php } ?>
 
-            </table>
+                    <?php
+
+                    if (active_heist($_SESSION['ID'], $pdo) && heist_row($heist_id, 'HEIST_type', $pdo) == 1 && heist_row($heist_id, 'HEIST_status', $pdo) == 1) {
+                        $crime_amount = 9;
+                    } else {
+                        $crime_amount = 8;
+                    }
+                    for ($i = 0; $i < $crime_amount; $i++) {
+
+                    ?>
+                        <tr class="cursor_hover clickable-row" data-href="?side=kriminalitet&alt=<?php echo $i; ?>">
+                            <td>Lorem ipsum </td>
+                            <td>Lorem ipsum s</td>
+                            <td>Lorem ipsum %</td>
+                            <td>Lorem ipsum </td>
+                        </tr>
+                    <?php } ?>
+
+                </table>
+            </div>
         </div>
     </div>
+    <div class="col-4" style="padding-top: 0px;">
+        <div class="content">
+            <form method="post">
+                <label for="time">Velg tidsperspektiv</label>
+                <select name="time" id="cars">
+                    <option value="1">Siste timen</option>
+                    <option value="2">Siste 2 timer</option>
+                    <option value="3">Siste 6 timer</option>
+                    <option value="4">Siste 12 timer</option>
+                    <option value="5">Siste 24 timer</option>
+                </select>
+                <input type="submit" name="update" value="Oppdater">
+            </form>
 
-    <br><br>
-    <b>Forklaring:</b>
-    <br>
-    <div style='float: left; height: 1px; width: 1px; padding: 5px 5px; background-color: rgba(50, 205, 50, 0.1); border-radius: 10px;'></div>Lite
-    <br>
-    <div style='float: left; height: 1px; width: 1px; padding: 5px 5px; background-color: rgba(50, 205, 50, 0.5); border-radius: 10px;'></div>Middels (Over 5 trykk på samme sted)
-    <br>
-    <div style='float: left; height: 1px; width: 1px; padding: 5px 5px; background-color: rgba(50, 205, 50, 1); border-radius: 10px;'></div>Mye (Risiko om bot (Over 10 trykk på samme sted))
-    <style>
-        #heatmap {
-            position: relative;
-        }
-    </style>
+
+            <br><br>
+            <b>Forklaring:</b>
+            <br>
+            <div class="dot" style='background-color: rgba(50, 205, 50, 0.1);'></div>Lite
+            <br>
+            <div class="dot" style='background-color: rgba(50, 205, 50, 0.5);'></div>Middels (Over 5 trykk på samme sted)
+            <br>
+            <div class="dot" style='background-color: rgba(50, 205, 50, 1);'></div>Mye (Stor risiko om bot)
+            <style>
+                .dot {
+                    float: left;
+                    height: 1px;
+                    width: 1px;
+                    padding: 5px 5px;
+                    border-radius: 10px;
+                    margin-right: 10px;
+                }
+
+                #heatmap {
+                    position: relative;
+                }
+            </style>
+        </div>
+
+    </div>
+
 
 <?php } ?>
