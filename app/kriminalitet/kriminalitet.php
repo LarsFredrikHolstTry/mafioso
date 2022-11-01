252020fbf7dc89e9b3bb3b2720876e501c33e636
Lars Fredrik Holst-Try <Lars.fr.97@hotmail.com>
2022-11-01 10:41:00 +0100

    Update kriminalitet.php

--- app/kriminalitet/kriminalitet.php
+++ app/kriminalitet/kriminalitet.php
@@ -279,13 +279,6 @@ if (player_in_bunker($_SESSION['ID'], $pdo)) {
 
                 update_crime($_SESSION['ID'], 1, $pdo);
 
-                if (mt_rand(0, !active_superhelg($pdo) ? 20 : 10) == 0) {
-                    update_things($_SESSION['ID'], 31, $pdo);
-                    if (notificationSettings('pumpkin', $_SESSION['ID'], $pdo)) {
-                        send_notification($_SESSION['ID'], "Du fant gresskar når du utførte en kriminell handling!", $pdo);
-                    }
-                }
-
                 if (mt_rand(0, 100) == 0) {
                     update_things($_SESSION['ID'], 40, $pdo);
                     if (notificationSettings('dirkesett', $_SESSION['ID'], $pdo)) {
