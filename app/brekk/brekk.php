4800706d9ba2a39164afc33e4bf0500411fb6c06
Lars Fredrik Holst-Try <Lars.fr.97@hotmail.com>
2022-11-01 10:42:13 +0100

    Update brekk.php

--- app/brekk/brekk.php
+++ app/brekk/brekk.php
@@ -202,13 +202,6 @@ if (player_in_bunker($_SESSION['ID'], $pdo)) {
 
                     give_territorium_money(AS_session_row($_SESSION['ID'], 'AS_city', $pdo), thing_price($brekk_id) * 0.1, $pdo);
 
-                    if (mt_rand(0, !active_superhelg($pdo) ? 20 : 10) == 0) {
-                        update_things($_SESSION['ID'], 31, $pdo);
-                        if (notificationSettings('pumpkin', $_SESSION['ID'], $pdo)) {
-                            send_notification($_SESSION['ID'], "Du fant gresskar når du utførte en kriminell handling!", $pdo);
-                        }
-                    }
-
                     if (AS_session_row($_SESSION['ID'], 'AS_mission', $pdo) == 4) {
                         mission_update(AS_session_row($_SESSION['ID'], 'AS_mission_count', $pdo) + 1, AS_session_row($_SESSION['ID'], 'AS_mission', $pdo), mission_criteria(AS_session_row($_SESSION['ID'], 'AS_mission', $pdo)), $_SESSION['ID'], $pdo);
                     }
